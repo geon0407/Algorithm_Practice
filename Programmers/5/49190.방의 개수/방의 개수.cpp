4a866a31ccedb91531a8c1b5f584e1ef64f2dd08
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-10 11:15:09 +0900

    [level 5] Title: 방의 개수, Time: 64.12 ms, Memory: 18.3 MB -BaekjoonHub

--- Programmers/5/49190.방의 개수/방의 개수.cpp
+++ Programmers/5/49190.방의 개수/방의 개수.cpp
@@ -8,25 +8,22 @@ int solution(vector<int> arrows) {
     int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
     int answer = 0;
 
-    int x=0, y=0;
+    pnt p = make_pair(0, 0);
     set<pnt> pchk;
     set<lin> lchk;
 
-    pchk.insert(make_pair(0, 0));
+    pchk.insert(p);
     for(auto a : arrows) {
         for(int i=0;i<2;i++) {
-            int nx, ny;
-            nx = x + dx[a];
-            ny = y + dy[a];
+            pnt np = make_pair(p.first + dx[a], p.second + dy[a]);
 
-            auto tmp1 = pchk.insert(make_pair(nx, ny));
-            auto tmp2 = lchk.insert(make_pair(max(make_pair(x, y) ,make_pair(nx, ny)), min(make_pair(x, y) ,make_pair(nx, ny))));
+            auto tmp1 = pchk.insert(np);
+            auto tmp2 = lchk.insert(make_pair(max(p, np), min(p, np)));
             if(!tmp1.second && tmp2.second) {
                 answer++;
             }
 
-            x = nx;
-            y = ny;
+            p = np;
         }
     }
 
