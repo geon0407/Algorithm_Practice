547671b0fca678836033dd66d9c7793ef14c5537
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-15 10:44:02 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ Programmers/0/120912.7의 개수/7의 개수.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    
+    for(auto i : array) {
+        while( i ) {
+            if(i % 10 == 7) {
+                answer++; 
+            }
+            i /= 10;
+        }
+    }
+    
+    return answer;
+}
