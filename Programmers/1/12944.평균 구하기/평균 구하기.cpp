5d5aff3d24463839b199105ba0bb78aa6ffb8586
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-15 14:35:56 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.79 MB -BaekjoonHub

---
+++ Programmers/1/12944.평균 구하기/평균 구하기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    
+    for(auto i : arr) {
+        answer += i;
+    }
+    
+    return answer/arr.size();
+}
