669d39fbada8cd2ea20f92499eef60a7faa680d8
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-15 11:12:13 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ Programmers/0/120837.개미 군단/개미 군단.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    
+    answer += hp / 5;
+    answer += (hp % 5) / 3;
+    answer += (hp % 5) % 3;
+    
+    return answer;
+}
