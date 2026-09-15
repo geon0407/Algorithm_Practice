e07c291d10ee1a69e7417140f9825673b0622cb9
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-15 11:02:06 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.63 MB -BaekjoonHub

---
+++ Programmers/0/120850.문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    
+    for(auto c : my_string) {
+        if(isdigit(c)) {
+            answer.push_back(c - '0');
+        }
+    }
+    
+    sort(answer.begin(), answer.end());    
+    return answer;
+}
