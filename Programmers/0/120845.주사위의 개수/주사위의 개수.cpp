fd1fa50e3214a5fe160505ba0ea08724d2d5ee5a
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-15 11:04:32 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ Programmers/0/120845.주사위의 개수/주사위의 개수.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> box, int n) {
+    int answer = 0;
+    
+    return answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
+}
