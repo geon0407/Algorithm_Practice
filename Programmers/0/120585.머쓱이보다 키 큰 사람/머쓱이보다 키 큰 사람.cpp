bce7251722c43440b504daab47fb75b8d6267a1c
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-15 11:31:56 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.86 MB -BaekjoonHub

---
+++ Programmers/0/120585.머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    
+    for(int i : array) {
+        if(i > height) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
