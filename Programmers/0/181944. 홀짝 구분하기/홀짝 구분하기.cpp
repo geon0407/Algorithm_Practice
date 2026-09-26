87c0156d48916ff41ae4ba4224fe08336815fdd8
어릴적꿈에 <53281908+geon0407@users.noreply.github.com>
2026-09-26 16:06:33 +0900

    [level 0] Title: 홀짝 구분하기, Time: 2.98 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ Programmers/0/181944. 홀짝 구분하기/홀짝 구분하기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    
+    cout << n << " is " << ((n % 2) ? "odd\n" : "even\n");
+    
+    return 0;
+}
